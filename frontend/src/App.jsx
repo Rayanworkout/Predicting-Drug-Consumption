a6f7e85b3fb288213b0ad1edfb4f73f9999952c5
Sharnalk <98692061+Sharnalk@users.noreply.github.com>
2024-04-26 17:21:51 +0200

    mise a jour page d'accueil (implémentation de la traduction + style) + swiper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import './assets/index.css'
 
 import React, {useEffect, useState} from "react";
 import Swiper from "./components/Swiper.jsx";
-import SortButton from "./components/SortButton.jsx";
 import TutorialSwiper from "@/components/TutorialSwiper.jsx";
 import {Separator} from "@/components/ui/separator.jsx";
+import useStore from "@/store/store.js";
 
 function App() {
+    const {translations} = useStore();
     const [dataArray, setDataArray] = useState([]);
     const fetchData = async () => {
         setDataArray([]);
@@ -17,25 +18,28 @@ function App() {
         }, 2000)
     }
     useEffect(() => {
-
         //Appel de l'API pour set dans dataArray pour l'affichage des graph dans les swiper
     }, []);
     return (
-    <>
-        <div className={`w-full h-full px-3 pb-3 overflow-hidden`}>
-                <div className = {`pt-20 flex flex-col items-center gap-y-5`} alt="Texte de présentation">
-                    <span className = {`pb-10 px-10 flex flex-col md:flex-row items-center`}>
-                        <p className={` text-6xl font-bold text-white md:text-7xl`}>
-                            Drug Analytics
+        <>
+            <div className={`w-full h-full px-3 pb-3 overflow-hidden`}>
+                <div className={`pt-20 flex flex-col items-center gap-y-5 overflow-hidden`} alt="Texte de présentation">
+
+                    <span className={`animate-glowSpinL absolute z-[0] w-[200px] h-[100px] bg-blue-400 left-0 mt-20 ml-20 rounded-full blur-3xl`}></span>
+                    <span className={`animate-glowSpinR absolute z-[0] w-[200px] h-[100px] bg-[#bec1ff] left-0 ml-32 md:ml-52 rounded-full blur-3xl`}></span>
+
+                    <span className={`pb-10 px-10 flex flex-col md:flex-row items-center`}>
+                        <p className={`text-6xl md:text-[5rem] font-bold text-white `}>
+                            {translations.main.title}
                         </p>
-                        <p className={`text-center text-white p-6`}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolor doloribus eligendi inventore iste libero minus molestiae non numquam possimus, quisquam quod rerum sit vel veniam veritatis voluptas voluptate voluptatum?
+                        <p className={`text-xlbase md:text-xl text-center text-white font-semibold pt-9 md:p-6`}>
+                            {translations.main.homeContent}
                         </p>
                     </span>
                     <TutorialSwiper/>
                 </div>
-            <Separator className="mt-16"/>
-            <span className={`flex pb-3 lg:px-20`}>
+                <Separator className="mt-16"/>
+                <span className={`flex pb-3 lg:px-20`}>
                 <span className={` flex flex-col gap-y-2 mt-20 mb-10 `}>
                     <h1 className={`
                            text-4xl 
@@ -51,18 +55,14 @@ function App() {
                     </h2>
                 </span>
             </span>
-            <div className={` w-full h-0 bg-white text-white`}></div>
-            <div className={` flex flex-col w-100 gap-y-8 text-white `}>
-                <Swiper swiperTitle={'Comparaison Chart'}/>
-                <Swiper swiperTitle={'Repartition Chart'}/>
-                <Swiper swiperTitle={'Correlation Chart'}/>
+                <div className={` w-full h-0 bg-white text-white`}></div>
+                <div className={` flex flex-col w-100 gap-y-8 text-white `}>
+                    <Swiper swiperTitle={'Comparaison Chart'}/>
+                    <Swiper swiperTitle={'Repartition Chart'}/>
+                    <Swiper swiperTitle={'Correlation Chart'}/>
+                </div>
             </div>
-
-            <div>
-
-            </div>
-        </div>
-    </>
+        </>
     )
 
 }
