eb194c06fbd1b49bd31191a25c472ebe7c7bcebb
Sharnalk <98692061+Sharnalk@users.noreply.github.com>
2024-04-26 17:19:40 +0200

    mise a jour de la nav bar (input pour chanegr la langue)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import Router from "@/router/router.jsx";
-import Header from "./components/Header.jsx";
+import Header from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
