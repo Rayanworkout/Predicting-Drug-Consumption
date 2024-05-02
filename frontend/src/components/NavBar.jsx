c3b7d02a46c8b758e820e1319d4d3649dd9566d3
Sharnalk <98692061+Sharnalk@users.noreply.github.com>
2024-05-02 15:46:59 +0200

    maj style

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { Separator } from "@/components/ui/separator.jsx";
 import drapeauFR from "@/assets/img/drapeau_fr.png";
 import drapeauEN from "@/assets/img/drapeau_en.png";
+import { Star } from 'lucide-react';
 
 const NavBar = () => {
     let { language, setLanguage } = useStore();
     const newLanguage = language === "en" ? "fr" : "en";
+    const t = useStore(state => state.translations);
 
     return (
         <div className={`flex w-full h-full text-white font-bold gap-3 pt-10 py-3 px-6 md:px-10 bg-transparent backdrop-blur justify-between align-baseline  z-50`}>
@@ -41,9 +43,9 @@ const NavBar = () => {
             target={"_blank"}
             rel="noopener noreferrer"
             href={"https://github.com/Rayanworkout/Predicting-Drug-Consumption"}
-            className=" my-auto text-sm font-medium rounded bg-[#333] px-2 py-1 shadow-md transition duration-150 ease-in-out hover:shadow-2xl hover:bg-[#444] focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg cursor-pointer"
-        >
-          <span className = {``}>Star us on GitHub !</span>
+            className="group rounded bg-[#333] px-2 py-1 transition duration-150 ease-in-out hover:bg-[#444] cursor-pointer">
+          <span className = {`flex items-center gap-x-1 text-sm font-medium`}>
+              <Star className = {`group-hover:fill-amber-300  transition ease-in-out duration-100000 h-[20px]`}/> <p>{t.main.git} </p></span>
         </a>
       </span>
         </div>
